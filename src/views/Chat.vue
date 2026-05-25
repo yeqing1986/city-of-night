66bcf866c0e315f801436ac03f37b7cc4a0e0771
Developer <dev@example.com>
2026-05-25 18:55:13 +0800

    fix: 修正起始节点ID为1-1-000

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -360,7 +360,7 @@ onMounted(async () => {
 async function startNewGame() {
   try {
     // 开始第一章第一节
-    await DialogueEngine.startDialogue('1-1', '1-1_msg_001')
+    await DialogueEngine.startDialogue('1-1', '1-1-000')
     
     // 显示第一条消息
     const currentMsg = DialogueEngine.getCurrentMessage()
